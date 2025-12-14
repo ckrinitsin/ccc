546c7e0bc1c6614cbe95934b4f8783a253caa91c
Christian Krinitsin <mail@krinitsin.com>
2025-12-14 14:56:14 +0100

    Add lexer tests

---
+++ tests/test_lex.rs
@@ -0,0 +1,62 @@
+use ccc::lex::{lex, Token};
+
+#[test]
+fn valid1() {
+    let input = "
+    int main(void) {
+        return 2;
+    }
+    ";
+
+    let result = lex(input.to_string()).unwrap();
+    let expected = vec![
+        Token::Int,
+        Token::Identifier("main".to_string()),
+        Token::OpenBrace,
+        Token::Void,
+        Token::CloseBrace,
+        Token::OpenParanthesis,
+        Token::Return,
+        Token::Constant(2),
+        Token::Semicolon,
+        Token::CloseParanthesis,
+    ];
+
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn valid2() {
+    let input = "
+    int main(void) {
+        return
+    }
+    ";
+
+    let result = lex(input.to_string()).unwrap();
+    let expected = vec![
+        Token::Int,
+        Token::Identifier("main".to_string()),
+        Token::OpenBrace,
+        Token::Void,
+        Token::CloseBrace,
+        Token::OpenParanthesis,
+        Token::Return,
+        Token::CloseParanthesis,
+    ];
+
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn invalid() {
+    let input = "
+    int main(void) {
+        123ab
+    }
+    ";
+
+    let result = lex(input.to_string());
+
+    assert!(result.is_err());
+}
