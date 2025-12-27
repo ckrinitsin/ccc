6a453e0975a3e095cb257241fddf6d293e76ff6f
Christian Krinitsin <mail@krinitsin.com>
2025-12-27 08:58:53 +0100

    Update Compiler Driver to allow multiple sources and compilation of libraries

--- src/cli.rs
+++ src/cli.rs
@@ -18,109 +18,146 @@ use std::process::Command;
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    /// Input c program
-    file: PathBuf,
+    /// Input c programs
+    files: Vec<PathBuf>,
 
     /// Run lexer and stop afterwards
-    #[arg(short, long)]
+    #[arg(long)]
     lex: bool,
 
     /// Run lexer and parser, stop afterwards
-    #[arg(short, long)]
+    #[arg(long)]
     parse: bool,
 
     /// Run lexer, parser and semantic analyzer, stop afterwards
-    #[arg(short, long)]
+    #[arg(long)]
     validate: bool,
 
     /// Run lexer, parser, semantic analyzer and ir, stop afterwards
-    #[arg(short, long)]
+    #[arg(long)]
     tacky: bool,
 
     /// Run lexer, parser, semantic analyzer, ir and codegen, stop afterwards
-    #[arg(short, long)]
+    #[arg(long)]
     codegen: bool,
+
+    /// Compiles and assemble the file, but do not link
+    #[arg(short, long)]
+    compile: bool,
+
+    /// Output file
+    #[arg(short, long)]
+    output: Option<PathBuf>,
 }
 
 pub fn cli() -> Result<()> {
     let args = Cli::parse();
 
-    let mut asm_file = args.file.clone();
-    _ = asm_file.set_extension("S");
-    let mut preprocessed_file = args.file.clone();
-    _ = preprocessed_file.set_extension("i");
-    let mut binary_file = args.file.clone();
-    _ = binary_file.set_extension("");
-
-    let output = Command::new("gcc")
-        .arg("-E")
-        .arg("-P")
-        .arg(args.file)
-        .arg("-o")
-        .arg(&preprocessed_file)
-        .output()?;
-
-    if !output.status.success() {
-        io::stdout().write_all(&output.stdout)?;
-        io::stderr().write_all(&output.stderr)?;
-        bail!("Could not preprocess file");
+    let mut binary_file;
+    if let Some(output_file) = args.output {
+        binary_file = output_file;
+    } else {
+        binary_file = args.files[0].clone();
+        _ = binary_file.set_extension("");
     }
 
-    let content = fs::read_to_string(&preprocessed_file)?;
-    fs::remove_file(preprocessed_file)?;
+    let mut asm_files = Vec::new();
+    for file in args.files {
+        let mut asm_file = file.clone();
+        _ = asm_file.set_extension("S");
+        let mut preprocessed_file = file.clone();
+        _ = preprocessed_file.set_extension("i");
 
-    let tokens = lex::lex(content)?;
+        let output = Command::new("gcc")
+            .arg("-E")
+            .arg("-P")
+            .arg(&file)
+            .arg("-o")
+            .arg(&preprocessed_file)
+            .output()?;
 
-    if args.lex {
-        println!("Tokens:");
-        for tok in tokens {
-            print!("{} ", tok);
+        if !output.status.success() {
+            io::stdout().write_all(&output.stdout)?;
+            io::stderr().write_all(&output.stderr)?;
+            bail!("Could not preprocess file");
         }
+
+        let content = fs::read_to_string(&preprocessed_file)?;
+        fs::remove_file(preprocessed_file)?;
+
+        let tokens = lex::lex(content)?;
+
+        if args.lex {
+            println!("Tokens for {:?}:", file);
+            for tok in tokens {
+                print!("{} ", tok);
+            }
+            continue;
+        }
+
+        let ast = parser::parse_tokens(tokens)?;
+
+        if args.parse {
+            println!("Ast for {:?}:", file);
+            println!("{:?}", ast);
+            continue;
+        }
+
+        let analyzed_ast = variable_resolution::variable_resolution(ast)?;
+        let analyzed_ast = label_resolution::label_resolution(analyzed_ast)?;
+        let analyzed_ast = loop_resolution::loop_resolution(analyzed_ast)?;
+
+        if args.validate {
+            println!("Analyzed Ast for {:?}:", file);
+            println!("{:?}", analyzed_ast);
+            continue;
+        }
+
+        let ir = ir::lift_to_ir(analyzed_ast)?;
+
+        if args.tacky {
+            println!("IR for {:?}:", file);
+            println!("{}", ir);
+            continue;
+        }
+
+        let asm = codegen::gen_asm(ir)?;
+
+        if args.codegen {
+            println!("Codegen for {:?}:", file);
+            println!("{}", asm);
+            continue;
+        }
+
+        let mut file = File::create(&asm_file)?;
+        write!(file, "{}", asm)?;
+
+        asm_files.push(asm_file);
+    }
+
+    if args.lex || args.parse || args.validate || args.tacky || args.codegen {
         return Ok(());
     }
 
-    let ast = parser::parse_tokens(tokens)?;
-
-    if args.parse {
-        println!("Ast:");
-        println!("{:?}", ast);
-        return Ok(());
+    let output;
+    if args.compile {
+        output = Command::new("gcc")
+            .arg("-c")
+            .args(&asm_files)
+            .arg("-o")
+            .arg(binary_file)
+            .output()?;
+    } else {
+        output = Command::new("gcc")
+            .args(&asm_files)
+            .arg("-o")
+            .arg(binary_file)
+            .output()?;
     }
 
-    let analyzed_ast = variable_resolution::variable_resolution(ast)?;
-    let analyzed_ast = label_resolution::label_resolution(analyzed_ast)?;
-    let analyzed_ast = loop_resolution::loop_resolution(analyzed_ast)?;
-
-    if args.validate {
-        println!("Analyzed Ast:");
-        println!("{:?}", analyzed_ast);
-        return Ok(());
+    for asm_file in asm_files {
+        fs::remove_file(asm_file)?;
     }
-
-    let ir = ir::lift_to_ir(analyzed_ast)?;
-
-    if args.tacky {
-        println!("{}", ir);
-        return Ok(());
-    }
-
-    let asm = codegen::gen_asm(ir)?;
-
-    if args.codegen {
-        println!("{}", asm);
-        return Ok(());
-    }
-
-    let mut file = File::create(&asm_file)?;
-    write!(file, "{}", asm)?;
-
-    let output = Command::new("gcc")
-        .arg(&asm_file)
-        .arg("-o")
-        .arg(binary_file)
-        .output()?;
-
-    fs::remove_file(asm_file)?;
 
     if !output.status.success() {
         io::stdout().write_all(&output.stdout)?;
